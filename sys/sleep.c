87e2dfd6e7489f1b22eb0e8f55b2cb1450613639
skumar32 <skumar32@ncsu.edu>
2020-01-12 23:04:35 -0500

    updated sleep.c

--- sys/sleep.c
+++ sys/sleep.c
@@ -6,20 +6,28 @@
 #include <q.h>
 #include <sleep.h>
 #include <stdio.h>
+#include <lab0.h>
 
 /*------------------------------------------------------------------------
  * sleep  --  delay the calling process n seconds
  *------------------------------------------------------------------------
  */
+extern long ctr1000;
+
 SYSCALL	sleep(int n)
-{
+{	
+	update_syscall_count(currpid,18);
+	unsigned long syscall_exec_start = ctr1000;
 	STATWORD ps;    
-	if (n<0 || clkruns==0)
+	if (n<0 || clkruns==0){
+		update_syscall_time(currpid,18,ctr1000 - syscall_exec_start);
 		return(SYSERR);
+	}
 	if (n == 0) {
 	        disable(ps);
 		resched();
 		restore(ps);
+		update_syscall_time(currpid,18,ctr1000 - syscall_exec_start);
 		return(OK);
 	}
 	while (n >= 1000) {
@@ -28,5 +36,7 @@ SYSCALL	sleep(int n)
 	}
 	if (n > 0)
 		sleep10(10*n);
+	update_syscall_time(currpid,18,ctr1000 - syscall_exec_start);
+	kprintf ("\n\n\n Debug from sleep %lu", ctr1000 - syscall_exec_start); 
 	return(OK);
 }
