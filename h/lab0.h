3a4ff5bf4a7c979acfee0afbaa526be6c0fead51
skumar32 <skumar32@ncsu.edu>
2020-01-12 23:03:23 -0500

    added lab0.h

---
+++ h/lab0.h
@@ -0,0 +1,5 @@
+/* lab0.h - isbadpid */
+unsigned int syscall_flag;
+
+extern void update_syscall_count(int pid, int syscall_index);
+extern void update_syscall_time(int pid, int syscall_index,unsigned long diff_time);
