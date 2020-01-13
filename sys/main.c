d6968765b166381aacc79554b31d89d012a1baaf
skumar32 <skumar32@ncsu.edu>
2020-01-12 23:04:09 -0500

    updated main.c

--- sys/main.c
+++ sys/main.c
@@ -10,7 +10,13 @@
  *------------------------------------------------------------------------
  *
  */
-
+int prX;
+prch(c)
+char c;
+{
+	int i;
+	sleep(5);	
+}
 int main()
 {
 	kprintf("\n\nHello World, Xinu lives\n\n");
@@ -18,5 +24,11 @@ int main()
 	kprintf("\n Task 1: Input = 0xaabbccdd; Output = 0x%x",o);
 	printsegaddress();
 	printprocstks(1);
+        printtos();
+	syscallsummary_start();
+	resume(prX = create(prch,2000,20,"proc X",1,'A'));
+	sleep(20);
+	syscallsummary_stop();
+	printsyscallsummary();
 	return 0;
 }
